5dfc998419721c94331b421aea1c61213f5f7b32
Raúl Santana Castilla <168531613+RaulSantanaCastilla@users.noreply.github.com>
2024-08-01 10:58:33 +0000

    Trabajo terminado

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 const MyJumbotron = () => (
   <div className="jumbotron">
     <div className="container">
-      <h1>¡Una cálida Bienvenida!</h1>
+      <h1>¡Una <strong>GIGANTESCA</strong> Bienvenida!</h1>
       <p>
         En esta página muestro una de mis películas favoritas de mi infancia y de mi vida en general.
       </p>
-      <a className="btn" href="#" role="button">¡Llamada a la acción!</a>
+      <a className="btn" href="#" role="button">¡Llamada a todos los robots!</a>
     </div>
   </div>
 );
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -6,17 +6,17 @@ const MyNavbar = () => {
   };
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Iniciar Bootstrap</div>
+      <div className="navbar-brand">El Gigante de Hierro</div>
       <div className="navbar-toggle" onClick={toggleNavbar}>
         <span></span>
         <span></span>
         <span></span>
       </div>
       <div className={`navbar-links ${isOpen ? 'show' : ''}`}>
-        <a className="navbar-link" href="#home">Inicio</a>
-        <a className="navbar-link" href="#features">Acerca de</a>
-        <a className="navbar-link" href="#pricing">Servicios</a>
-        <a className="navbar-link" href="#pricing">Contacto</a>
+        <a className="navbar-link" href="https://www.youtube.com/watch?v=5dfsOV0diP8" target="_blank" rel="noopener noreferrer">Película</a>
+        <a className="navbar-link" href="https://www.youtube.com/watch?v=-O7E21ZWm2E" target="_blank" rel="noopener noreferrer">Curiosidades</a>
+        <a className="navbar-link" href="https://www.redbubble.com/es/shop/el+gigante+de+hierro" target="_blank" rel="noopener noreferrer">Merchandising</a>
+        <a className="navbar-link" href="https://multiversus.com/es-es" target="_blank" rel="noopener noreferrer">Juegos</a>
       </div>
     </nav>
   );
